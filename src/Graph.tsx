5d155ed3bfa6c042d02b13aff86f919de469369a
tomgallego13 <93276519+tomgallego13@users.noreply.github.com>
2023-01-02 14:20:08 -0500

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -48,11 +48,13 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
       // Add more Perspective configurations here.
+      // adding attributes to update the graph
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top_ask_price"]');
+      //specifically updates the graph without duplicates 
       elem.setAttribute('aggregates', '{"stock":"distinct count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct count"}');
     }
   }
